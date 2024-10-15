d93fae8786253f65c2819dd6239ff79391690b50
julia <168024513+juliraven@users.noreply.github.com>
2024-10-16 00:48:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title(':tomato: Analiza sentymentu recenzji filmowych na podstawie recenzji uzytkowników strony Rotten Tomatoes :tomato:')
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <h2>Wyrównany tekst do środka</h2>
+        <p>To jest przykładowy tekst wyśrodkowany na stronie.</p>
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
