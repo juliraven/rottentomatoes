e18789ab4dcd0f453eebea697e455462b49a1605
julia <168024513+juliraven@users.noreply.github.com>
2024-10-16 00:56:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import streamlit as st
 
 st.markdown(
     """
+    <style>
+    .emoji-top {
+        margin-top: -20px; /* Zmniejszenie marginesu górnego */
+    }
+    </style>
+    
     <div style="text-align: center;">
-        <h1>🍅<h1>
-        <h1>🍅 Analiza sentymentu recenzji filmowych na podstawie recenzji użytkowników strony Rotten Tomatoes 🍅</h1>
+        <h1 class="emoji-top">🍅</h1> 
+        <h2>🍅 Analiza sentymentu recenzji filmowych na podstawie recenzji użytkowników strony Rotten Tomatoes 🍅</h2>
     </div>
     """, 
     unsafe_allow_html=True
