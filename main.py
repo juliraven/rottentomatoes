a46ad09779473f309569c0b403c3c6f83a33308b
julia <168024513+juliraven@users.noreply.github.com>
2024-10-16 00:53:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import streamlit as st
 st.markdown(
     """
     <div style="text-align: center;">
-        <h1>Analiza sentymentu recenzji filmowych na podstawie recenzji użytkowników strony Rotten Tomatoes</h1>
-        <h2>🍅 🍅 🍅<h2>
+        <h1>🍅 Analiza sentymentu recenzji filmowych na podstawie recenzji użytkowników strony Rotten Tomatoes 🍅</h1>
     </div>
     """, 
     unsafe_allow_html=True
