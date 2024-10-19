54183584cd49e4cb50d07595a2b4b6dd61c6b840
julia <168024513+juliraven@users.noreply.github.com>
2024-10-20 01:10:31 +0200

    Update 1_🍅 Analizy.py

--- pages/1_🍅 Analizy.py
+++ pages/1_🍅 Analizy.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-dane = pd.read_excel('dane1.csv', header=1)
+dane = pd.read_csv('dane1.csv', header=1)
 
 sentiment_counts = dane['sentiment'].value_counts().reset_index()
 sentiment_counts.columns = ['sentiment', 'count']
