775b02c97f0e81c01f9f0183ab1dd393edf085f0
julia <168024513+juliraven@users.noreply.github.com>
2024-10-20 01:00:28 +0200

    Update 1_🍅 Analizy.py

--- pages/1_🍅 Analizy.py
+++ pages/1_🍅 Analizy.py
@@ -5,6 +5,15 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-dane = pd.read_excel('dane.csv', header=1)
+dane = pd.read_excel('dane1.csv', header=1)
 
+sentiment_counts = dane['sentiment'].value_counts().reset_index()
+sentiment_counts.columns = ['sentiment', 'count']
+
+fig = px.pie(sentiment_counts, 
+             values='count', 
+             names='sentiment', 
+             title='Podział recenzji wg sentymentu')
+
+st.plotly_chart(fig, use_container_width=True)
 
