a7b820ddde6ed6b918f1ed09cb8501f590041717
julia <168024513+juliraven@users.noreply.github.com>
2024-10-20 00:53:21 +0200

    Update 1_🍅 Analizy.py

--- pages/1_🍅 Analizy.py
+++ pages/1_🍅 Analizy.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+dane = pd.read_excel('dane.csv', header=1)
+
 
