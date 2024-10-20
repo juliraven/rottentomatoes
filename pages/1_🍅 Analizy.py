43947c80a5c3bff8176d0d1829c2979978c45425
julia <168024513+juliraven@users.noreply.github.com>
2024-10-20 02:05:02 +0200

    Update 1_🍅 Analizy.py

--- pages/1_🍅 Analizy.py
+++ pages/1_🍅 Analizy.py
@@ -8,7 +8,6 @@ import plotly.express as px
 
 dane1 = pd.read_csv('dane1.csv')
 dane2 = pd.read_csv('dane2.csv')
-dane3 = pd.read_csv('dane3.csv')
 dane4 = pd.read_csv('dane4.csv')
 
 sentiment_counts = dane1['sentiment'].value_counts().reset_index()
