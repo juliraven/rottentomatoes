2643fc8509d5f617ba66beecd04b04104f24802f
julia <168024513+juliraven@users.noreply.github.com>
2024-10-20 17:50:37 +0200

    Update 2_🍅 Ocena modeli.py

--- pages/2_🍅 Ocena modeli.py
+++ pages/2_🍅 Ocena modeli.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from wordcloud import WordCloud
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import plotly.express as px
 
+
+st.markdown(
+    """
+    <style>
+    .emoji-top {
+        margin-top: -20px; /* Zmniejszenie marginesu górnego */
+    }
+    </style>
+    
+    <div style="text-align: left;">
+        <h1>🍅 Ocena modeli</h1>
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
