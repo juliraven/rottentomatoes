90390d872c8026a32264e982c38634877f9e7309
julia <168024513+juliraven@users.noreply.github.com>
2024-10-20 22:07:24 +0200

    Update 1_🍅 Analiza zbioru danych.py

--- pages/1_🍅 Analiza zbioru danych.py
+++ pages/1_🍅 Analiza zbioru danych.py
@@ -28,70 +28,75 @@ st.markdown(
 
 st.markdown('###')
 
-st.markdown('### Chmury słów dla recenzji o danym sentymencie')
-c1, c2, c3 = st.columns((2,2,2))
-c1.image("negatywne.png", caption="Chmura słów dla recenzji o negatywnym sentymencie")
-c2.image("pozytywne.png", caption="Chmura słów dla recenzji o pozytywnym sentymencie")
-c3.image("neutralne.png", caption="Chmura słów dla recenzji o neutralnym sentymencie")
+tab1, tab2, tab3 = st.tabs(["Wykresy", "Ranking"])
 
-st.markdown('###')
+with tab1:
 
-a1, a2 = st.columns((2,2))
-sentiment_counts = dane1['sentiment'].value_counts().reset_index()
-sentiment_counts.columns = ['sentiment', 'count']
+    st.markdown('### Chmury słów dla recenzji o danym sentymencie')
+    c1, c2, c3 = st.columns((2,2,2))
+    c1.image("negatywne.png", caption="Chmura słów dla recenzji o negatywnym sentymencie")
+    c2.image("pozytywne.png", caption="Chmura słów dla recenzji o pozytywnym sentymencie")
+    c3.image("neutralne.png", caption="Chmura słów dla recenzji o neutralnym sentymencie")
 
-custom_colors = ['forestgreen', 'indianred', 'deepskyblue']  
+    st.markdown('###')
 
-fig = px.pie(sentiment_counts, 
+    a1, a2 = st.columns((2,2))
+    sentiment_counts = dane1['sentiment'].value_counts().reset_index()
+    sentiment_counts.columns = ['sentiment', 'count']
+
+    custom_colors = ['forestgreen', 'indianred', 'deepskyblue']  
+
+    fig = px.pie(sentiment_counts, 
              values='count', 
              names='sentiment', 
              width=800,  
              height=500,
              color_discrete_sequence=custom_colors)
 
-fig.update_traces(textfont_size=20)
-a1.markdown('### Podział recenzji wg sentymentu')
-a1.plotly_chart(fig, use_container_width=True)
+    fig.update_traces(textfont_size=20)
+    a1.markdown('### Podział recenzji wg sentymentu')
+    a1.plotly_chart(fig, use_container_width=True)
 
-custom_colors1 = ['red', 'rgb(61,94,47)']  
+    custom_colors1 = ['red', 'rgb(61,94,47)']  
 
-fig1 = px.pie(dane4, 
+    fig1 = px.pie(dane4, 
              values='review_type', 
              names='index', 
              width=800,  
              height=500,
              color_discrete_sequence=custom_colors1)
 
-fig1.update_traces(textfont_size=20)
-a2.markdown('### Podział recenzji wg typu')
-a2.plotly_chart(fig1, use_container_width=True)
-st.markdown('###')
+    fig1.update_traces(textfont_size=20)
+    a2.markdown('### Podział recenzji wg typu')
+    a2.plotly_chart(fig1, use_container_width=True)
+    st.markdown('###')
 
-gatunki = dane2['genres'].unique()
-wybierz_gatunek = st.selectbox('Wybierz gatunek :', gatunki)
-filtered_df = dane2[dane2['genres'] == wybierz_gatunek]
-filtered_df1 = dane3[dane3['genres'] == wybierz_gatunek]
-b1, b2 = st.columns((2,2))
+    gatunki = dane2['genres'].unique()
+    wybierz_gatunek = st.selectbox('Wybierz gatunek :', gatunki)
+    filtered_df = dane2[dane2['genres'] == wybierz_gatunek]
+    filtered_df1 = dane3[dane3['genres'] == wybierz_gatunek]
+    b1, b2 = st.columns((2,2))
 
-fig2 = px.pie(filtered_df1, values='count', names='sentiment', color='sentiment',
+    fig2 = px.pie(filtered_df1, values='count', names='sentiment', color='sentiment',
              width=800,  
              height=500,
              color_discrete_sequence=custom_colors)
 
-fig2.update_traces(textfont_size=20)
-b1.markdown('### Podział recenzji wg sentymentu dla wybranego gatunku')
-b1.plotly_chart(fig2, use_container_width=True)
+    fig2.update_traces(textfont_size=20)
+    b1.markdown('### Podział recenzji wg sentymentu dla wybranego gatunku')
+    b1.plotly_chart(fig2, use_container_width=True)
 
-fig3 = px.pie(filtered_df, values='count', names='review_type', color='review_type',
+    fig3 = px.pie(filtered_df, values='count', names='review_type', color='review_type',
              width=800,  
              height=500,
              color_discrete_sequence=custom_colors1)
 
-fig3.update_traces(textfont_size=20)
-b2.markdown('### Podział recenzji wg typu dla wybranego gatunku')
-b2.plotly_chart(fig3, use_container_width=True)
+    fig3.update_traces(textfont_size=20)
+    b2.markdown('### Podział recenzji wg typu dla wybranego gatunku')
+    b2.plotly_chart(fig3, use_container_width=True)
 
-
+with tab2:
+    st.header("cos")
 
 
 
