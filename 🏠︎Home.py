658a5a9e7967d20646487c53ace15a6e3889be5a
julia <168024513+juliraven@users.noreply.github.com>
2024-10-20 17:08:53 +0200

    Update 🏠︎Home.py

--- 🏠︎Home.py
+++ 🏠︎Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_pages import add_page_title, get_nav_from_toml
 
 st.set_page_config(page_title="Analiza Sentymentu")
 
@@ -21,3 +22,16 @@ st.markdown(
 st.image('tomato.png', use_column_width=True)
 
 
+
+
+sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
+
+nav = get_nav_from_toml(
+    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
+)
+
+pg = st.navigation(nav)
+
+add_page_title(pg)
+
+pg.run()
