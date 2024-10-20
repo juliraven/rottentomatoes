aa52cce25d3a7afe278e2c73579e3cc51707812c
julia <168024513+juliraven@users.noreply.github.com>
2024-10-20 17:22:19 +0200

    Update 1_🍅 Analiza zbioru danych.py

--- pages/1_🍅 Analiza zbioru danych.py
+++ pages/1_🍅 Analiza zbioru danych.py
@@ -16,14 +16,15 @@ custom_colors = ['forestgreen', 'indianred', 'deepskyblue']
 fig = px.pie(sentiment_counts, 
              values='count', 
              names='sentiment', 
-             title='Podział recenzji wg sentymentu',
              width=800,  
              height=500,
              color_discrete_sequence=custom_colors)
 
 fig.update_traces(textfont_size=20)
+st.title('Podział recenzji wg sentymentu')
 st.plotly_chart(fig, use_container_width=True)
 
+st.title('Chmury słów dla recenzji o danym sentymencie')
 st.image("negatywne.png", caption="Chmura słów dla recenzji o negatywnym sentymencie")
 st.image("pozytywne.png", caption="Chmura słów dla recenzji o pozytywnym sentymencie")
 st.image("neutralne.png", caption="Chmura słów dla recenzji o negutralnym sentymencie")
