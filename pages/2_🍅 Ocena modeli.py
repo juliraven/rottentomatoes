62777ec2bb0cc9c0ce90f935fabbb8c4adc8e661
julia <168024513+juliraven@users.noreply.github.com>
2024-10-22 00:31:20 +0200

    Update 2_🍅 Ocena modeli.py

--- pages/2_🍅 Ocena modeli.py
+++ pages/2_🍅 Ocena modeli.py
@@ -22,28 +22,6 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-from streamlit_option_menu import option_menu
 
-# Menu przypominające zakładki
-selected = option_menu(
-    menu_title=None,  # Tytuł menu, jeśli nie jest potrzebny, ustaw na None
-    options=["Tab 1", "Tab 2", "Tab 3"],  # Zakładki
-    icons=["house", "gear", "envelope"],  # Ikony dla zakładek (opcjonalne)
-    menu_icon="cast",  # Ikona menu (opcjonalne)
-    default_index=0,  # Domyślnie zaznaczona zakładka
-    orientation="horizontal",  # Układ poziomy (horizontal) lub pionowy (vertical)
-)
-
-# Warunkowe wyświetlanie treści w zależności od aktywnej zakładki
-if selected == "Tab 1":
-    st.write("This is Tab 1")
-    st.sidebar.empty()  # Brak sidebaru dla Tab 1
-elif selected == "Tab 2":
-    st.write("This is Tab 2")
-    with st.sidebar:
-        st.write("This is the sidebar for Tab 2")  # Sidebar tylko dla Tab 2
-elif selected == "Tab 3":
-    st.write("This is Tab 3")
-    st.sidebar.empty()  # Brak sidebaru dla Tab 3
 
 
