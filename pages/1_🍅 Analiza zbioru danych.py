277e55b7f389e3008b733212f603d6367066195d
julia <168024513+juliraven@users.noreply.github.com>
2024-10-22 00:53:25 +0200

    Update 1_🍅 Analiza zbioru danych.py

--- pages/1_🍅 Analiza zbioru danych.py
+++ pages/1_🍅 Analiza zbioru danych.py
@@ -39,7 +39,7 @@ if selected == "Wykresy":
     dane3 = pd.read_csv('dane3.csv')
     dane4 = pd.read_csv('dane4.csv')
 
-    st.markdown('###')
+    st.markdown('######')
     
     st.markdown('### Chmury słów dla recenzji o danym sentymencie')
     c1, c2, c3 = st.columns((2,2,2))
