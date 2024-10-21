143f073f354c6804d1358096c2f7c3989ffd4470
julia <168024513+juliraven@users.noreply.github.com>
2024-10-21 23:59:35 +0200

    Update 1_🍅 Analiza zbioru danych.py

--- pages/1_🍅 Analiza zbioru danych.py
+++ pages/1_🍅 Analiza zbioru danych.py
@@ -99,89 +99,86 @@ with tab1:
 
 
 
-with tab2:
-    st.markdown('### Ranking filmów')
+
+tab2.markdown('### Ranking filmów')
     
-    def load_data(file1, file2):
-        df1 = pd.read_csv(file1)
-        df2 = pd.read_csv(file2)
+def load_data(file1, file2):
+    df1 = pd.read_csv(file1)
+    df2 = pd.read_csv(file2)
     
-        df = pd.concat([df1, df2], ignore_index=True)
-        return df
+    df = pd.concat([df1, df2], ignore_index=True)
+    return df
 
-    file1 = 'dane5_1.csv'
-    file2 = 'dane5_2.csv'
+file1 = 'dane5_1.csv'
+file2 = 'dane5_2.csv'
 
 
-    df = load_data(file1, file2)
-    df['original_release_date'] = pd.to_datetime(df['original_release_date'])
+df = load_data(file1, file2)
+df['original_release_date'] = pd.to_datetime(df['original_release_date'])
     
-    tab2.sidebar.header('Opcje filtrowania')
+tab2.sidebar.header('Opcje filtrowania')
 
-    date_filter = tab2.sidebar.slider(
+date_filter = tab2.sidebar.slider(
     'Wybierz zakres dat (rok premiery) :',
     min_value=df['original_release_date'].min().date(),
     max_value=df['original_release_date'].max().date(),
     value=(df['original_release_date'].min().date(), df['original_release_date'].max().date())
-    )
+)
 
-    tomatometer_filter = st.sidebar.slider('Wybierz ocenę krytyków (Tomatometer) :', 
+tomatometer_filter = tab2.sidebar.slider('Wybierz ocenę krytyków (Tomatometer) :', 
                                        int(df['tomatometer_rating'].min()), 
                                        int(df['tomatometer_rating'].max()), 
                                        (int(df['tomatometer_rating'].min()), int(df['tomatometer_rating'].max())))
 
 
-    audience_filter = st.sidebar.slider('Wybierz ocenę widowni :', 
+audience_filter = tab2.sidebar.slider('Wybierz ocenę widowni :', 
                                     int(df['audience_rating'].min()), 
                                     int(df['audience_rating'].max()), 
                                     (int(df['audience_rating'].min()), int(df['audience_rating'].max())))
 
-    sentiment_filter = st.sidebar.multiselect('Wybierz sentyment :', options=df['sentiment'].unique(), default=df['sentiment'].unique())
+sentiment_filter = tab2.sidebar.multiselect('Wybierz sentyment :', options=df['sentiment'].unique(), default=df['sentiment'].unique())
 
 
-    filtered_df = df[(df['original_release_date'].between(pd.to_datetime(date_filter[0]), pd.to_datetime(date_filter[1]))) &
+filtered_df = df[(df['original_release_date'].between(pd.to_datetime(date_filter[0]), pd.to_datetime(date_filter[1]))) &
                  (df['tomatometer_rating'].between(tomatometer_filter[0], tomatometer_filter[1])) &
                  (df['audience_rating'].between(audience_filter[0], audience_filter[1])) &
                  (df['sentiment'].isin(sentiment_filter))]
 
 
-    aggregated_df = filtered_df.groupby('movie_title').agg(
+aggregated_df = filtered_df.groupby('movie_title').agg(
     {
         'original_release_date': 'first',  # Zachowujemy datę premiery
         'tomatometer_rating': 'mean',       # Średnia ocena krytyków
         'audience_rating': 'mean'         # Średnia ocena widowni
-    }
-    ).reset_index()
+    }).reset_index()
 
 
-    sorted_df = aggregated_df.sort_values(by=['audience_rating', 'tomatometer_rating'], ascending=False)
+sorted_df = aggregated_df.sort_values(by=['audience_rating', 'tomatometer_rating'], ascending=False)
     sorted_df['original_release_date']=sorted_df['original_release_date'].astype(str).str[:10]
     sorted_df = sorted_df.rename(columns={
     'movie_title': 'Tytuł filmu',
     'original_release_date': 'Data premiery',
     'tomatometer_rating': 'Ocena krytyków',
-    'audience_rating': 'Ocena widowni',
-    })
+    'audience_rating': 'Ocena widowni'})
     
+max_tomatometer = sorted_df['Ocena krytyków'].max()
+max_audience = sorted_df['Ocena widowni'].max()
 
-    max_tomatometer = sorted_df['Ocena krytyków'].max()
-    max_audience = sorted_df['Ocena widowni'].max()
+def create_bar(value, max_value, color):
+    bar_length = (value / max_value) * 100 
+    return f"<div style='width: 100%; background-color: #e0e0e0;'><div style='width: {bar_length}%; background-color: {color}; height: 10px;'></div></div>"
 
-    def create_bar(value, max_value, color):
-        bar_length = (value / max_value) * 100 
-        return f"<div style='width: 100%; background-color: #e0e0e0;'><div style='width: {bar_length}%; background-color: {color}; height: 10px;'></div></div>"
+html_table = "<table style='width:100%; border-collapse: collapse;'>"
+html_table += "<tr><th>Tytuł filmu</th><th>Data Premiery</th><th>Ocena krytyków</th><th>Ocena widowni</th><th>Tomatometer</th><th>Audience</th></tr>"
 
-    html_table = "<table style='width:100%; border-collapse: collapse;'>"
-    html_table += "<tr><th>Tytuł filmu</th><th>Data Premiery</th><th>Ocena krytyków</th><th>Ocena widowni</th><th>Tomatometer</th><th>Audience</th></tr>"
+for index, row in sorted_df.iterrows():
+    tomatometer_bar = create_bar(row['Ocena krytyków'], max_tomatometer, 'magenta')
+    audience_bar = create_bar(row['Ocena widowni'], max_audience, 'blue')
+    html_table += f"<tr><td>{row['Tytuł filmu']}</td><td>{row['Data premiery']}</td><td>{row['Ocena krytyków']}</td><td>{row['Ocena widowni']}</td><td>{tomatometer_bar}</td><td>{audience_bar}</td></tr>"
 
-    for index, row in sorted_df.iterrows():
-        tomatometer_bar = create_bar(row['Ocena krytyków'], max_tomatometer, 'magenta')
-        audience_bar = create_bar(row['Ocena widowni'], max_audience, 'blue')
-        html_table += f"<tr><td>{row['Tytuł filmu']}</td><td>{row['Data premiery']}</td><td>{row['Ocena krytyków']}</td><td>{row['Ocena widowni']}</td><td>{tomatometer_bar}</td><td>{audience_bar}</td></tr>"
+html_table += "</table>"
 
-    html_table += "</table>"
-
-    st.markdown(html_table, unsafe_allow_html=True)
+tab2.markdown(html_table, unsafe_allow_html=True)
 
 
 
