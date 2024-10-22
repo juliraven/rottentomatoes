9c3eae3315159ba9e7c09ecdbd4aaca8af67503b
julia <168024513+juliraven@users.noreply.github.com>
2024-10-23 00:58:01 +0200

    Update 3_🍅 Testowanie modeli.py

--- pages/3_🍅 Testowanie modeli.py
+++ pages/3_🍅 Testowanie modeli.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+from sklearn.metrics import confusion_matrix
 
+st.markdown(
+    """
+    <style>
+    .emoji-top {
+        margin-top: -20px; /* Zmniejszenie marginesu górnego */
+    }
+    </style>
+    
+    <div style="text-align: left;">
+        <h1>🍅 Testowanie modeli</h1>
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
