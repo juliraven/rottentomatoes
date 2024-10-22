ccb59c375a5770ba018061136d28e333f53eca62
julia <168024513+juliraven@users.noreply.github.com>
2024-10-23 01:24:02 +0200

    Update 2_🍅 Ocena modeli.py

--- pages/2_🍅 Ocena modeli.py
+++ pages/2_🍅 Ocena modeli.py
@@ -51,7 +51,7 @@ with tab1:
     st.plotly_chart(fig)
 
     st.subheader('Trafność :')
-    st.info(f'**Trafność: {round(trafnosc * 100, 3)}%**')
+    st.success(f'**Trafność: {round(trafnosc * 100, 3)}%**')
 
 with tab2:
 
