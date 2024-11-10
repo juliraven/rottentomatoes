e70c2463df84feddef87d027feef6fe6d1bdcf59
julia <168024513+juliraven@users.noreply.github.com>
2024-11-10 05:30:44 +0100

    Update 4_🍅 Inne.py

--- pages/4_🍅 Inne.py
+++ pages/4_🍅 Inne.py
@@ -3,30 +3,22 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-url = st.text_input("Podaj link do recenzji na RT", "https://www.rottentomatoes.com/m/terrifier_3/reviews")
+url = st.text_input("Podaj link do recenzji na RT :", "https://www.rottentomatoes.com/m/terrifier_3/reviews")
 
-# Sprawdzamy, czy użytkownik wprowadził URL
 if url:
-    # Wysłanie żądania HTTP do podanego URL
     res = requests.get(url)
     
-    # Sprawdzenie, czy odpowiedź jest poprawna
     if res.status_code == 200:
-        # Przetworzenie zawartości strony za pomocą BeautifulSoup
         content = BeautifulSoup(res.content, 'html.parser')
-        
-        # Wyszukanie recenzji
+    
         reviews = content.find_all('p', class_='review-text')
         
-        # Wyodrębnienie tekstu recenzji
         review_texts = [review.get_text(strip=True) for review in reviews]
         
-        # Sprawdzenie, czy recenzje zostały znalezione
         if review_texts:
+            reviews_df = pd.DataFrame(review_texts, columns=['review_content'])
             st.write(f"Znaleziono {len(review_texts)} recenzji:")
-            for i, review in enumerate(review_texts, start=1):
-                st.write(f"**Recenzja {i}:** {review}")
-                st.write("---")  # Separator między recenzjami
+            st.dataframe(reviews_df)
         else:
             st.write("Nie znaleziono recenzji na tej stronie.")
     else:
