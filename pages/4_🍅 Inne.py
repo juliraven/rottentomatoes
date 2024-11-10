1c07432bb25c33e67698cfc184d0707c2a64a26b
julia <168024513+juliraven@users.noreply.github.com>
2024-11-10 05:11:00 +0100

    Update 4_🍅 Inne.py

--- pages/4_🍅 Inne.py
+++ pages/4_🍅 Inne.py
@@ -6,3 +6,6 @@ from bs4 import BeautifulSoup
 url = st.text_input("Podaj link do recenzji na RT", "https://www.rottentomatoes.com/m/terrifier_3/reviews")
 
 res = requests.get(url)
+
+content = BeautifulSoup(res.content, 'html.parser')
+st.code(content)
