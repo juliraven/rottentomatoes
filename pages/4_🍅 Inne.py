d0831da233037774ce8271edc452970d5c6e6b51
julia <168024513+juliraven@users.noreply.github.com>
2024-11-10 05:56:08 +0100

    Update 4_🍅 Inne.py

--- pages/4_🍅 Inne.py
+++ pages/4_🍅 Inne.py
@@ -17,8 +17,20 @@ if url:
     
     if res.status_code == 200:
         content = BeautifulSoup(res.content, 'html.parser')
+
+        title_tag = content.find('a', class_='sidebar-title')
+        title = title_tag.get_text(strip=True) if title_tag else "Nie znaleziono tytułu"
         
-        reviews = content.find_all('div', class_='review-text')
+        image_tag = content.find('a', href_='img src')
+        image_url = image_tag['img src'] if image_tag else None
+        
+        st.write(f"**Tytuł:** {title}")
+        if image_url:
+            st.image(image_url, caption="Obrazek", use_column_width=True)
+        else:
+            st.write("Nie znaleziono obrazka.")
+        
+        reviews = content.find_all('p', class_='review-text')
         
         review_texts = [review.get_text(strip=True) for review in reviews]
         if review_texts:
