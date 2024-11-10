6b606c210708d08ddf76aa8658aaed5c7b45c93f
julia <168024513+juliraven@users.noreply.github.com>
2024-11-10 05:07:32 +0100

    Update 4_🍅 Inne.py

--- pages/4_🍅 Inne.py
+++ pages/4_🍅 Inne.py
@@ -3,4 +3,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+st.write("Podaj link")
+url = st.text_input("Enter Rotten Tomatoes reviews page URL", "https://www.rottentomatoes.com/m/terrifier_3/reviews")
+
 
