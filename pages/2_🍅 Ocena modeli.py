c0084e10b769202e2a9bed7f85e36e9e3224d473
julia <168024513+juliraven@users.noreply.github.com>
2024-12-12 18:29:32 +0100

    Update 2_🍅 Ocena modeli.py

--- pages/2_🍅 Ocena modeli.py
+++ pages/2_🍅 Ocena modeli.py
@@ -43,10 +43,11 @@ with tab1:
     specificity_rounded = round(specificity * 100)
     precision_rounded = round(precision * 100)
 
+    st.markdown('### Macierz pomyłek')
+
     col1, col2 = st.columns([3, 1])  
     
     with col1:
-        st.markdown('### Macierz pomyłek')
     
         fig = px.imshow(cm, labels=dict(x="Przewidywane", y="Rzeczywiste", color="Liczność"), 
                     x=['Negatywne', 'Pozytywne'], 
