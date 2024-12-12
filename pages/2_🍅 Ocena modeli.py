353962ae97b356aa57acbf0149d1c7cca5ad4524
julia <168024513+juliraven@users.noreply.github.com>
2024-12-12 18:48:47 +0100

    Update 2_🍅 Ocena modeli.py

--- pages/2_🍅 Ocena modeli.py
+++ pages/2_🍅 Ocena modeli.py
@@ -59,7 +59,7 @@ with tab1:
                       font=dict(size=16),
                       margin=dict(t=70, b=0),
                       coloraxis_showscale=False,
-                      title="Macierz pomyłek", title_x=0.62, title_xanchor="center", title_font=dict(size=30, family="Arial, sans-serif", color="white"))
+                      title="Macierz pomyłek", title_x=0.6, title_xanchor="center", title_font=dict(size=30, family="Arial, sans-serif", color="white"))
 
         st.plotly_chart(fig)
 
