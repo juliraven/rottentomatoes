ecd0d4d543dade4048bbc09b1a0753bed7f4a70f
julia <168024513+juliraven@users.noreply.github.com>
2024-12-12 20:16:40 +0100

    Update 4_🍅 Testowanie modeli - opcja 2.py

--- pages/4_🍅 Testowanie modeli - opcja 2.py
+++ pages/4_🍅 Testowanie modeli - opcja 2.py
@@ -207,38 +207,7 @@ if selected == "Naiwny klasyfikator Bayesa":
     
 if selected == "Sieć neuronowa":
     
-    url = st.text_input("Podaj link do recenzji na RT:")
-
-    if url:
-        res = requests.get(url)
-    
-        if res.status_code == 200:
-            content = BeautifulSoup(res.content, 'html.parser')
-
-            title_tag = content.find('a', class_='sidebar-title')
-            title = title_tag.get_text(strip=True) if title_tag else "Nie znaleziono tytułu"
-        
-            image_tag = content.find('rt-img', {'data-qa': 'sidebar-poster-img'}) 
-            image_url = image_tag['src'] if image_tag else None
-        
-            col1, col2 = st.columns([1, 2])
-            if image_url:
-                col1.write(f"**Tytuł:** {title}")
-                col1.image(image_url, width=280)
-            else:
-                col1.write("Nie znaleziono obrazka.")
-        
-            reviews = content.find_all('p', class_='review-text')
-        
-            review_texts = [review.get_text(strip=True) for review in reviews]
-            if review_texts:
-                for review in review_texts:
-                    st.write(f"**Recenzja:**\n{review}")
-                    st.markdown("---")
-            else:
-                st.write("Nie znaleziono recenzji na tej stronie.")
-        else:
-            st.write("Nie udało się pobrać strony. Sprawdź, czy adres URL jest poprawny.")
+    st.markdown('#')
 
 
 
