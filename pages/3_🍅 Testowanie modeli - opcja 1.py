c0c8dc7cf8ad00572c9424b6602c22c246caeff2
julia <168024513+juliraven@users.noreply.github.com>
2024-12-24 01:41:53 +0100

    Update 3_🍅 Testowanie modeli - opcja 1.py

--- pages/3_🍅 Testowanie modeli - opcja 1.py
+++ pages/3_🍅 Testowanie modeli - opcja 1.py
@@ -166,7 +166,8 @@ if selected == "Naiwny klasyfikator Bayesa":
 
     st.markdown("### Analiza sentymentu dla recenzji użytkownika")
 
-    user_review = st.text_area("Wprowadź swoją recenzję w języku angielskim tutaj:")
+    user_review = st.text_area("Wprowadź swoją recenzję w języku angielskim tutaj:", 
+                               placeholder="Przykład recenzji: 'The second season’s high-octane story reveals an even deeper emotional core, painting a beautiful canvas of family and turning its initial solemn tone into one of hope, building on everything so admired about season one.'")
 
     if st.button('Analizuj sentyment'):
         if user_review:
@@ -208,9 +209,9 @@ if selected == "Sieć neuronowa":
 
     st.markdown("### Analiza sentymentu dla recenzji użytkownika")
 
-    user_review = st.text_area("Wprowadź swoją recenzję w języku angielskim tutaj:")
+    user_review = st.text_area("Wprowadź swoją recenzję w języku angielskim tutaj:", 
+                               placeholder="Przykład recenzji: 'The second season’s high-octane story reveals an even deeper emotional core, painting a beautiful canvas of family and turning its initial solemn tone into one of hope, building on everything so admired about season one.'")
 
-    
 
     max_length = 30 
 
