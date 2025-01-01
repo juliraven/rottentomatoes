e4483c585bf18a78c052bd24792c08549570c483
julia <168024513+juliraven@users.noreply.github.com>
2025-01-01 18:20:21 +0100

    Update 🏠︎Home.py

--- 🏠︎Home.py
+++ 🏠︎Home.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+
 
 st.set_page_config(page_title="Analiza sentymentu", layout="wide")
 
