2e8eb24ed99de19870ce767052f961b4a318a59b
julia <168024513+juliraven@users.noreply.github.com>
2025-01-07 23:12:08 +0100

    Update 🏠︎Strona_główna.py

--- 🏠︎Strona_główna.py
+++ 🏠︎Strona_główna.py
@@ -18,7 +18,7 @@ st.sidebar.markdown(
     unsafe_allow_html=True
 )
 
-add_logo('logo.png', height=50)
+add_logo('logo.png', height=10)
 st.sidebar.image('tomato.png', width=300)
 
 page_bg_img = """
