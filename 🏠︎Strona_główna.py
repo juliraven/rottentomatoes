c76abea8d0d9da00f9dac04bed5f793aaecdf759
julia <168024513+juliraven@users.noreply.github.com>
2025-01-08 00:00:58 +0100

    Update 🏠︎Strona_główna.py

--- 🏠︎Strona_główna.py
+++ 🏠︎Strona_główna.py
@@ -3,7 +3,7 @@ from streamlit_extras.app_logo import add_logo
 
 st.set_page_config(page_title="Analiza sentymentu", page_icon="🎥", layout="wide")
 
-add_logo('nowe-logo.png', height=350)
+add_logo('nowe-logo.png', height=400)
 
 st.sidebar.markdown(
     """
