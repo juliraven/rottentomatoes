20a2ace8d263675ded400666473fee560ce82f01
julia <168024513+juliraven@users.noreply.github.com>
2025-01-08 00:39:25 +0100

    Update 1_🍅 Analiza zbioru danych.py

--- pages/1_🍅 Analiza zbioru danych.py
+++ pages/1_🍅 Analiza zbioru danych.py
@@ -24,6 +24,44 @@ header[data-testid="stHeader"] {
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
+add_logo('logo.png', height=350)
+
+st.sidebar.markdown(
+    """
+    <style>
+        [data-testid="stSidebar"] {
+            padding-top: 0px;
+            padding: 10px;
+            font-family: sans-serif;
+            font-size: 18px;
+        }
+
+        [data-testid="stSidebarHeader"] {
+            height: 30px;
+            padding: 5px 10px; 
+            margin: 0; 
+            display: flex; 
+            align-items: center;
+            justify-content: center; 
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"] {
+        width: 350px;  /* Ustaw stałą szerokość */
+        min-width: 350px;  /* Minimalna szerokość */
+        max-width: 350px;  /* Maksymalna szerokość */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.markdown(
     """
     <style>
