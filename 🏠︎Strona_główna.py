198923899ce07f47e2338c8f101e97ecc718db58
julia <168024513+juliraven@users.noreply.github.com>
2025-01-08 00:32:10 +0100

    Update 🏠︎Strona_główna.py

--- 🏠︎Strona_główna.py
+++ 🏠︎Strona_główna.py
@@ -9,10 +9,7 @@ st.sidebar.markdown(
     """
     <style>
         [data-testid="stSidebar"] {
-            margin-top: 0px auto
-        }
-
-        [data-testid="stSidebar"] {
+            margin-top: 10px;
             padding: 10px;
             font-family: sans-serif;
             font-size: 18px;
