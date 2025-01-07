448c58d321f9017de7db7ba12912eec58072fc71
julia <168024513+juliraven@users.noreply.github.com>
2025-01-08 00:26:16 +0100

    Update 🏠︎Strona_główna.py

--- 🏠︎Strona_główna.py
+++ 🏠︎Strona_główna.py
@@ -9,7 +9,7 @@ st.sidebar.markdown(
     """
     <style>
         [data-testid="stSidebar"]::before {
-            margin-top: 0px auto
+            margin: 0px auto
         }
 
         [data-testid="stSidebar"] {
