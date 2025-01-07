335b920cf992c905b342c9295187109668f12626
julia <168024513+juliraven@users.noreply.github.com>
2025-01-07 23:07:50 +0100

    Update 🏠︎Strona_główna.py

--- 🏠︎Strona_główna.py
+++ 🏠︎Strona_główna.py
@@ -18,7 +18,7 @@ st.sidebar.markdown(
     unsafe_allow_html=True
 )
 
-st.logo('tomato.png', size='large')
+st.logo('logo.png', size='large')
 st.sidebar.image('tomato.png', width=300)
 
 page_bg_img = """
